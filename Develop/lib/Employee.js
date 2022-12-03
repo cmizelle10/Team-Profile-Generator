f1dbf8e118f2d477228f75a85702592f5211ba53
Chris <cmizelle10@gmail.com>
2022-12-03 18:42:18 -0500

    adding test for employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -21,16 +21,3 @@ class Employee {
 
 module.exports = Employee;
 
-// class Shape {
-//     constructor(area, perimeter) {
-//       this.area = area;
-//       this.perimeter = perimeter;
-//     }
-  
-//     printInfo() {
-//       console.log(`Area: ${this.area}`);
-//       console.log(`Perimeter: ${this.perimeter}`);
-//     }
-//   }
-  
-//   module.exports = Shape;
